0b9b82c7b7094f893f9dc2527e6e1eb66aa76109
Angus Gratton <gus@projectgus.com>
2024-07-01 19:02:48 +1000

    gear_ratios: Add rpm graph

--- gear_ratios/gear_ratios.py
+++ gear_ratios/gear_ratios.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 class Motor:
     def torque(self, rpm):
+        """ Returns motor torque in Nm at a given rpm. """
         raise NotImplementedError
 
     def power(self, rpm):
-        # Returns kW
+        """ Returns motor power in kW for a given rpm. """
         return self.torque(rpm) * math.pi * rpm / 30 / 1000
 
 
@@ -20,6 +21,8 @@ class EVMotor(Motor):
         self.max_rpm = max_rpm
 
     def torque(self, rpm):
+        # Assume an "ideal" AC motor, constant torque up to a max power point
+        # then torque linearly degrades to the max RPM point which has 0 torque.
         if rpm < self.max_power_rpm:
             return self.max_torque  # in max torque part of the graph
         elif rpm > self.max_rpm:
@@ -37,7 +40,27 @@ class KonaMotor(EVMotor):
         super().__init__(151, 395, 3600, 10500)
 
 
-class D4D1KD(Motor):
+class ICEEngine(Motor):
+    def __init__(self):
+        pass
+
+    def torque(self, rpm):
+        min_rpm, min_torque = self._T[0]
+        if rpm < min_rpm:
+            # "slip" the clutch when starting from below idle rpm...
+            return max(.1, rpm / min_rpm) * min_torque
+
+        # Otherwise interpolate between two torque points
+        for (a_r, a_t), (b_r, b_t) in zip(self._T, self._T[1:]):
+            if a_r <= rpm <= b_r:
+                r = (rpm - a_r) / (b_r - a_r)
+                return (r * b_t) + ((1 - r) * a_t)
+
+        # Above redline?
+        return 0
+
+
+class D4D1KD(ICEEngine):
     # rpm to torque, according to
     # https://www.4x4community.co.za/forum/showthread.php/10949-2-5-D4D-vs-3-0-D4D
     _T = [
@@ -48,15 +71,18 @@ class D4D1KD(Motor):
         (5000, 90),
     ]
 
-    def __init__(self):
-        pass
 
-    def torque(self, rpm):
-        for (a_r, a_t), (b_r, b_t) in zip(self._T, self._T[1:]):
-            if a_r <= rpm <= b_r:
-                r = (rpm - a_r) / (b_r - a_r)
-                return (r * b_t) + ((1 - r) * a_t)
-        return 0
+class I43RZFE(ICEEngine):
+    # rpm to torque, according to
+    # https://www.advrider.com/f/threads/a-toyota-based-adventure-rig-brainstorming.831138/page-2
+    _T = [
+        (1000, 175),
+        (1800, 205),
+        (2000, 220),
+        (4000, 230),
+        (4500, 220),
+        (5200, 195),
+        ]
 
 
 class Wheel:
@@ -102,11 +128,15 @@ class Vehicle:
     def wheel_torque(self, kph):
         # Return the torque at the wheels for a given speed, assuming best
         # gear selected
+        gear = self.select_gear(kph)
+        motor_rpm = self.get_motor_rpm(kph)
+        return self.engine.torque(motor_rpm) * self.gears[gear] * self.final_drive
+
+    def get_motor_rpm(self, kph):
         wheel_rpm = self.wheel.wheel_rpm(kph)
         driveshaft_rpm = wheel_rpm * self.final_drive
         gear = self.select_gear(kph)
-        engine_rpm = driveshaft_rpm * self.gears[gear]
-        return self.engine.torque(engine_rpm) * self.gears[gear] * self.final_drive
+        return driveshaft_rpm * self.gears[gear]
 
     def torque_data(self):
         # making use of implicit x axis of len(ys)
@@ -116,30 +146,60 @@ class Vehicle:
         # making use of implicit x axis of len(ys)
         return [self.select_gear(kph) for kph in range(0, 121)]
 
+    def motor_rpm_data(self):
+        # making use of implicit x axis of len(ys)
+        return [self.get_motor_rpm(kph) for kph in range(0, 121)]
+
 
 stock_wheels = Wheel(205, 70, 15)
-stock_hilux = Vehicle(
+stock_td_hilux = Vehicle(
     D4D1KD(), stock_wheels, [4.313, 2.330, 1.436, 1.000, 0.838], 3.583
 )
+
+
+# This car doesn't exist as this series Hilux as the 2TR-FE engine,
+# but I couldn't find a power curve for that one. The 3RZ-FE is the predecessor
+# with similar overall specs but maybe no variable valve timing.
+stock_i4_hilux = Vehicle(
+    I43RZFE(), stock_wheels, [3.830, 2.062, 1.436, 1.000, 0.838], 4.1)
 
 kona_wheels = Wheel(215, 55, 17)
 kona = Vehicle(KonaMotor(), kona_wheels, [1.0], 7.981)
 
 conv_hilux_3rd = Vehicle(KonaMotor(), stock_wheels, [1.436], 3.583)
 conv_hilux_2nd = Vehicle(KonaMotor(), stock_wheels, [2.330], 3.583)
+#conv_hilux_petrol_2nd = Vehicle(KonaMotor(), stock_wheels, [2.062], 3.583)  # diesel diff
+
 conv_hilux_3rd_41diff = Vehicle(KonaMotor(), stock_wheels, [1.436], 4.1)
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2, 1)
 
-handles = (ax.plot(kona.torque_data(), label="Kona Electric") +
-           ax.plot(stock_hilux.torque_data(), label="Stock Hilux 3.0 D4D, 5sp manual") +
-           ax.plot(conv_hilux_2nd.torque_data(), label="Electric Hilux, fixed 2nd") +
-           ax.plot(conv_hilux_3rd.torque_data(), label="Electric Hilux, fixed 3rd") +
-           ax.plot(conv_hilux_3rd_41diff.torque_data(), label="Electric Hilux, fixed 3rd, 4.1 diff")
+handles = (ax1.plot(kona.torque_data(), label="Kona Electric") +
+           ax1.plot(stock_td_hilux.torque_data(), label="Stock Hilux 3.0 D4D, 5sp manual") +
+           ax1.plot(stock_i4_hilux.torque_data(), label="Stock-ish Hilux 2.7, 5sp manual") +
+           ax1.plot(conv_hilux_2nd.torque_data(), label="Electric Hilux, locked in 2nd (diesel gearbox)") +
+           #ax1.plot(conv_hilux_petrol_2nd.torque_data(), label="Electric Hilux, fixed petrol 2nd (diesel diff)") +
+           ax1.plot(conv_hilux_3rd.torque_data(), label="Electric Hilux, locked in 3rd") +
+           ax1.plot(conv_hilux_3rd_41diff.torque_data(), label="Electric Hilux, locked in 3rd, 4.1 diff")
            )
-ax.legend(handles=handles)
+ax1.set_xlabel("Speed (km/h)")
+ax1.set_ylabel("Wheel Torque (Nm)")
+ax1.legend(handles=handles)
 
-#plt.subplot(212)
-#plt.plot(stock_hilux.gear_data(), label="Stock Hilux 3.0 D4D, gear")
+ax2.set_xlabel("Speed (km/h)")
+ax2.set_ylabel("Motor RPM")
+dashes = [1, 2]
+handles = (ax2.plot(kona.motor_rpm_data(), dashes=dashes) +
+           ax2.plot(stock_td_hilux.motor_rpm_data(), dashes=dashes) +
+           ax2.plot(stock_i4_hilux.motor_rpm_data(), dashes=dashes) +
+           ax2.plot(conv_hilux_2nd.motor_rpm_data(), dashes=dashes) +
+           #ax2.plot(conv_hilux_petrol_2nd.motor_rpm_data(), dashes=dashes) +
+           ax2.plot(conv_hilux_3rd.motor_rpm_data(), dashes=dashes) +
+           ax2.plot(conv_hilux_3rd_41diff.motor_rpm_data(), dashes=dashes)
+           )
 
+ax1.grid(True, linestyle=':')
+ax2.grid(True, linestyle=':')
+
+fig.tight_layout()
 plt.show()
